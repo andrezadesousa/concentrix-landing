7484d34cc5f344c132f2ddf838e51dfc3247a9bf
Andreza Sousa <andrezanascimentodesousa@gmail.com>
2025-10-20 14:25:54 -0300

    FAQ - hidden in the answers added

--- src/components/FaqCards.tsx
+++ src/components/FaqCards.tsx
@@ -1,17 +1,30 @@
+"use client";
+import { useState } from "react";
+
 interface FaqProps {
   title: string;
   text: string;
 }
 
 function FaqCards({ title, text }: FaqProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div className="faq__card">
       <div className="faq__card-container">
-        <div className="faq__card-title">
+        <div
+          className="faq__card-title"
+          onClick={toggleAccordion}
+          style={{ cursor: "pointer" }}
+        >
           <span>{title}</span>
-          <i className="bxr bx-plus" />
+          <i className={`bxr ${isOpen ? "bx-minus" : "bx-plus"}`} />
         </div>
-        <p className="faq__card-text">{text}</p>
+        {isOpen && <p className="faq__card-text">{text}</p>}
       </div>
     </div>
   );
